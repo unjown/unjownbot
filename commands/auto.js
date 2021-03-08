c92be7e372e66ce7c4eeb5d5f031cbfcead3d743
unjown <windalelorenztan@yahoo.com>
2021-03-08 15:36:40 +0800

    lol
    lol

---
+++ commands/auto.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');  
+const prefix = "lol"
+
+const register = new Discord.MessageEmbed()
+    .setTitle('HOW TO REGISTER')
+    .setColor('#ffB35c')
+	.addFields(
+    {name: 'STEP 1', value: 'type /register in game and the your pin'},
+    {name: 'STEP 2', value: 'type a?register XXXX(your pin) in <#791886317491191818>'}, 
+    )
+
+
+module.exports = async (message) => { 
+    if (message.author.bot) return;
+
+    if(message.content.toLowerCase().includes('how' && 'register')){
+        message.channel.send(register)
+    }
+
+};
